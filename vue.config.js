f7869357eaa2e8b4ff5d2ce97e796b2927a94a29
Anthony Gore <anthony@vuejsdevelopers.com>
2020-06-23 07:47:29 +0700

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const path = require("path");
 module.exports = {
   devServer: {
     proxy: `http://localhost:${process.env.SERVER_PORT}`
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set("vue$", path.resolve("node_modules/vue/dist/vue.esm-bundler.js"));
   }
 };
